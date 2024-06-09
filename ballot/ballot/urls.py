b09356d2101b2467166169219ac2257fced47383
David S.M <davidsanchezmotran@gmail.com>
2024-06-09 19:09:02 -0300

    ADD index publico de resultados

--- ballot/ballot/urls.py
+++ ballot/ballot/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from voting.views import IndexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('admin_black.urls')),
     path('__reload__/', include('django_browser_reload.urls')),
+    path('', IndexView.as_view(), name='index'),
     path('voting/', include('voting.urls')),
 ]
 
--- ballot/voting/views.py
+++ ballot/voting/views.py
@@ -3,6 +3,15 @@ from django.views.generic import TemplateView
 from voting.models import PoliticalParty
 
 
+class IndexView(TemplateView):
+    template_name = 'voting/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['political_parties'] = PoliticalParty.objects.all()
+        return context
+
+
 class PoliticalPartyView(TemplateView):
     template_name = 'voting/political-party.html'
 
